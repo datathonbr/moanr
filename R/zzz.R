1113723489a5942d35f5a14940e38abb80dcffc4
jtrecenti <julio.trecenti@gmail.com>
2018-03-30 00:00:27 -0300

    all files and scripts

--- R/zzz.R
+++ R/zzz.R
@@ -5,13 +5,13 @@
     beepr::beep(f)
   }
 
-  mostrar_a_piroca <- function() {
-    img <- readRDS(system.file("negao-da-piroca.rds", package = "moanr"))
-    par(mar=c(0,0,0,0))
-    plot(as.raster(img))
-  }
+  # mostrar_a_piroca <- function() {
+  #   img <- readRDS(system.file("negao-da-piroca.rds", package = "moanr"))
+  #   par(mar=c(0,0,0,0))
+  #   plot(as.raster(img))
+  # }
 
   s <- sort(sample(1:500, 10, replace = FALSE))
   sapply(s, function(x) later::later(gemer, x))
-  sapply(s-2, function(x) later::later(mostrar_a_piroca, x))
+  # sapply(s-2, function(x) later::later(mostrar_a_piroca, x))
 }
